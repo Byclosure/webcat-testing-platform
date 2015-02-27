2ff360f6fd04888994c114c81f625ce5ba39374b
Bernardo Oliveira Pinto <bernardoopinto@gmail.com>
2015-02-27 13:06:34 +0000

    Update websteps test suite

--- src/test/java/com/byclosure/webcat/stepdefinitions/WebStepsSuite.java
+++ src/test/java/com/byclosure/webcat/stepdefinitions/WebStepsSuite.java
@@ -5,6 +5,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({SeleniumInteractionsClickLink.class})
+@Suite.SuiteClasses({SeleniumInteractionsClickLink.class, SeleniumInteractionsSearchForContentByText.class})
 public class WebStepsSuite {
 }
